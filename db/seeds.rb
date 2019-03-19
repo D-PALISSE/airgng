3640c1ae3a9da1ae4ae94ab68e7c2ec8b17caa7f
David Palisse <palisse.david44@gmail.com>
2019-03-19 12:02:38 +0100

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning DB...'
+Goat.destroy_all
+
+puts 'Creating new goats'
+
+# TODO: REMPLIR LES CHAMPS VIDE CI-DESOUS
+
+david = User.create!(password: "123456")
+thierry = User.create!(password: "123456")
+matthias = User.create!(password: "123456")
+nassim = User.create!(password: "123456")
+
+species = %w[]
+Goat.create!(name: "Jacqueline", specie: , details: , owner: david, photo: File.open(Rails.root.join('db/fixtures/goats/chevre_1.jpg')))
+Goat.create(name: "Magdalena", specie: , details: , photo:)
+Goat.create(name: "Josette", specie: , details: , photo:)
+Goat.create(name: "Dounia", specie: , details: , photo:)
+Goat.create(name: "Sabrina", specie: , details: , photo:)
+Goat.create(name: "Diana", specie: , details: , photo:)
+Goat.create(name: "Carline", specie: , details: , photo:)
+Goat.create(name: "Charley", specie: , details: , photo:)
+Goat.create(name: "Margaux", specie: , details: , photo:)
+Goat.create(name: "Clotilde", specie: , details: , photo:)
+Goat.create(name: "Myrtille", specie: , details: , photo:)
+Goat.create(name: "Angela", specie: , details: , photo:)
+Goat.create(name: "Sandra", specie: , details: , photo:)
+
+Boooking.create()
+
+puts 'Finished!'
