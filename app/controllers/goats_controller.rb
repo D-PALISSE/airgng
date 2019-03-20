df2e275d528d54c06bbf441cc59cb0123f1a32ae
thierry DE AZEVEDO <tshlda07911@gmail.com>
2019-03-20 16:23:36 +0100

    add view of goat details

--- app/controllers/goats_controller.rb
+++ app/controllers/goats_controller.rb
@@ -1,6 +1,22 @@
 class GoatsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
+  # before_action :set_goat, only: [:show]
+
   def index
     @goats = Goat.all
+  end
+
+  def show
+    @goat = Goat.find(params[:id])
+  end
+
+  private
+
+  # def set_goat
+  #   @goat = Goat.find(params[:id])
+  # end
+
+  def goat_params
+    params.require(:goat).permit(:name, :address, :specie, :description, :photo, :daily_price)
   end
 end
