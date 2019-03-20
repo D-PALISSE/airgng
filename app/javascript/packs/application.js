3df498f442ce1de297a36eccb247b5a017773ea9
David Palisse <palisse.david44@gmail.com>
2019-03-20 01:19:03 +0100

    changed navbar text

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,11 @@
 import "bootstrap";
+
+// const navbarOnglets = document.querySelectorAll("inactive");
+// const ongletActiv = document.querySelector('.active');
+
+// navbarOnglets.forEach((onglet) => {
+//   onglet.addEventListener(("click"), (event) => {
+//   onglet.classList.add("active")
+//   ongletActiv.classList.remove("active")
+//   });
+// });
