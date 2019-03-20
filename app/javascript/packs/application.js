d70953754b92ba6961774735973946dcd8b5d11a
Nassim Boukhalfa <nassim106@hotmail.com>
2019-03-20 16:43:05 +0100

    Booking controller et page new booking avec calendrier intégré + calcul dynamique du total amount. Manque finalisation côté back
    Signed-off-by: Nassim Boukhalfa <nassim106@hotmail.com>

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,29 @@
 import "bootstrap";
+import flatpickr from 'flatpickr'
+import 'flatpickr/dist/flatpickr.min.css'
+
+flatpickr(".datepicker", {
+  mode: "range",
+  onChange: function(selectedDates, dateStr, instance) {
+    // console.log(dateStr)
+    // console.log(selectedDates)
+    // console.log(selectedDates[selectedDates.length-1])
+    const periodChosen = selectedDates.length == 2
+    if (periodChosen) {
+      const totalAmountSpan = document.getElementById('total_amount')
+
+      const beginningDate = selectedDates[0]
+      const endDate = selectedDates[selectedDates.length-1]
+
+      const goatDailyPrice = instance.element.dataset.dailyPrice
+
+      const daysCount = (endDate - beginningDate) / 60 / 60 / 24 / 1000
+
+      totalAmountSpan.innerText = daysCount * goatDailyPrice
+      const beginningDateInput = document.getElementById('booking_beginning_date')
+      const endDateInput = document.getElementById('booking_end_date')
+      beginningDateInput.value = beginningDate
+      endDateInput.value = endDate
+    }
+  },
+})
